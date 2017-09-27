cc88810d0e9d82d4475bf25b2867f0e8382a446a
jaeseokbyun <31755186+jaeseokbyun@users.noreply.github.com>
2017-09-27 14:21:13 +0900

    정적변수 정적메서드

---
+++ src/lesson3/ClassBase1.java
@@ -0,0 +1,27 @@
+package lesson3;
+
+public class ClassBase1 {
+     static public void main(String[] arg){
+                     Constructor c = new Constructor();
+                     System.out.println(c.getName());
+                     System.out.println(c.getFriend(0));
+                     Constructor c1 = new Constructor("kiwan");
+                     System.out.println(c1.getName());
+                     System.out.println(c1.getFriend(0));
+                     Constructor c2 = new Constructor("kiwan", "maeng");
+                     System.out.println(c2.getName());
+                     System.out.println(c2.getFriend(0));
+
+
+                     NoConst no = new NoConst();
+                     System.out.println(no.getFirstName(c2));
+
+
+                     System.out.println(Constructor.plus(3, 5));
+                     //기본은 클래스이름.정적메소드()
+                     System.out.println(c2.plus(5, 5));
+                 //인스턴스에서도 정적메소드 호출이 된다
+                 }
+}
+
+
---
+++ src/lesson3/NoConst.java
@@ -0,0 +1,8 @@
+package lesson3;
+
+public class NoConst {
+    public String getFirstName(Constructor c){
+                return c.firstName.substring(0, 2) + "...";
+             }
+
+}
