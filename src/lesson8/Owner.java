1236c7eee37e9e20421efc7efd9472b1fc34d9c3
jaeseokbyun <31755186+jaeseokbyun@users.noreply.github.com>
2017-11-10 13:51:05 +0900

    20171110-1

--- src/lesson8/Owner.java
+++ src/lesson8/Owner.java
@@ -1,16 +1,22 @@
 package lesson8;
-
 class Owner {
-    // 공개될 필요가 없다면 , 가장 범위가 적은 수준으로.==> 생일.
-    // final  class,method에 걸면 extends와 override 불능
-    // 점진적인
-    //
-    // 확장 구현 ==> 게임 버전 업 패치
-    // 상속을 막는 원리; 디커플링; 택배기사의 전화번호 원리. 택배기사는 interface만 사용.
-    // 값에 대해서는 private ,
-    // 다 설명할 수 있어야 해.
-    // break문은 유일한 예외...
-    // continue 문은 ddd
+    private final int ownerValue;
+    private final Base ownerBase;
+
+    Owner(int ownerValue, Base ownerBase) {
+        this.ownerValue = ownerValue;
+        this.ownerBase = ownerBase;
+    }
+
+    @Override
+    protected Owner clone() throws CloneNotSupportedException {
+        //얕은 복사
+        Owner result= new Owner(this.ownerValue, this.ownerBase);
+        //깊은 복사
+        //단일 책임 원칙. 각각의 객체는 자기 것만 짐.
+        result = new Owner(this.ownerValue,this.ownerBase.clone());
+        return result;
+    }
 }
 
 
